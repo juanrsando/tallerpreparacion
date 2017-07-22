0a5f8c8db0a11ac6f947e15e9e8d40ac607fadf6
juanrsando <jrsando_96@hotmail.com>
2017-07-22 14:07:41 -0500

    cambios de base de datos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def one
   end
+  def new
+    Student.create(name: params[:name],email: params[:email])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#one'
   get 'pages/one'
+  post 'pages/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
